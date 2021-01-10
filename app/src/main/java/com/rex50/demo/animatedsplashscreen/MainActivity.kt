4f1761dca10c84f99025b55728294909cba0e1ef
Pavitra <pareshraut50@gmail.com>
2021-01-10 19:24:01 +0530

    @init - First commit

---
+++ app/src/main/java/com/rex50/demo/animatedsplashscreen/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rex50.demo.animatedsplashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
